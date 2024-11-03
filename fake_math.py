9ca801f40218679fe62101324749a7d031a555f5
Bulatique33 <ikhsanov.bulat@gmail.com>
2024-11-03 15:52:32 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,9 @@
+def devide(first,second):
+    if second == 0:
+        return 'Ошибка'
+    else:
+        return first/second
+
+
+# result = devide (69,3)
+# print(result)
---
+++ true_math.py
@@ -0,0 +1,12 @@
+from math import inf
+
+
+def devide(first,second):
+    if second == 0:
+        return inf
+    else:
+        return first/second
+
+
+# result = devide (69,1)
+# print(result)
